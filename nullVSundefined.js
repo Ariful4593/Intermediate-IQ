2e94b18f1408e15c6e47b96e495ee0b5cdf32693
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-30 00:27:28 +0600

    added

---
+++ nullVSundefined.js
@@ -0,0 +1,16 @@
+// // let name;
+// // // exapected output will be undefined;
+// // console.log(name)
+
+// function add(num1,num2){
+//     console.log(num1,num2)
+//     // exapected output will be undefined;
+// }
+// const result = add(10);
+// console.log(result)
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+// var lines = input.split('\n');
+// console.log(input)
+// process.stdin.on('data', function(data){
+//     console.log(data.toString());
+// })
---
+++ truthyFalsy.js
@@ -0,0 +1,20 @@
+//if string is fullfill then result will be Truthy else it will goes Falsy
+//if value is undefined then it will be Falsy
+//if value is null then it will be Falsy
+// Falsy
+// 0
+// ""
+// null
+// NaN
+// undefined
+// Truthy
+// '0' " "
+const name = "Ariful Islam";
+
+if(name)
+{
+    console.log("condition is True");
+}
+else{
+    console.log("condition is False")
+}
